d77d5d4fc26cb20facc50574eccccf2deaacfccf
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-04-14 17:13:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+
+function setup() 
+{
+  createCanvas(500,700);
+  engine = Engine.create();
+  world = engine.world;
+ 
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+}
+
+function draw() 
+{
+  background(51);
+
+  push();
+  imageMode(CENTER);
+  if(fruit!=null){
+    image(food,fruit.position.x,fruit.position.y,70,70);
+  }
+  pop();
+
+  rope.show();
+  Engine.update(engine);
+  ground.show();
+   
+}
+
+
+
+
